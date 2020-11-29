5db57438a2a9caf01cfa8e7b32a0451d3e7f1039
airamaeay <airamaearanas@gmail.com>
2020-11-29 21:47:40 +0800

    first commit

---
+++ staffs/login.php
@@ -0,0 +1,11 @@
+<?php require "../config.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<input type="text" name="user" placeholder="user">
+	<input type="password" name="password" placeholder="password">
+</body>
+</html>
