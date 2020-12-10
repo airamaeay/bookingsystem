a3d413c994ed249a9cdfba2e185f5416c0d5477d
A Star Vlog <admin@astarvlog.com>
2020-12-10 22:50:48 +0800

    navbar enhanced padding

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
             display: inline;
             width: 84px;
       }
-      .navbar-custom{
+      .remove-padding{
           padding:0px !important;
       }
   </style>
@@ -46,9 +46,9 @@
 <body>
 
   <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-    <div class="container head-custom">
-      <a class="navbar-brand" href="#"> <img class="logo" src="resources/images/logo-mini.png"> Booking System</a>
+  <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top remove-padding">
+    <div class="container">
+      <a class="navbar-brand remove-padding" href="#"> <img class="logo" src="resources/images/logo-mini.png"> Booking System</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
