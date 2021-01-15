62088bc2c24355bf3111a4be20c111f688eda176
A Star Vlog <admin@astarvlog.com>
2021-01-15 17:38:54 +0800

    rand

---
+++ test-rand.php
@@ -0,0 +1,15 @@
+<?php
+    function genRan(){return rand((int)10000000,(int)99999999);}
+    $random_numbers1 = genRan();
+    $random_numbers2 = genRan();
+    echo $random_numbers1;
+    echo "<br>";
+    echo $random_numbers1;
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo rand((int)10000000,(int)99999999);
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo rand(10000000,99999999);
