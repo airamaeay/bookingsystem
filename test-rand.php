5ea041b7c1b9503e4871614f38caff53fa99d5b5
A Star Vlog <admin@astarvlog.com>
2021-01-15 17:39:39 +0800

    rand

--- test-rand.php
+++ test-rand.php
@@ -3,13 +3,13 @@
     $random_numbers1 = genRan();
     $random_numbers2 = genRan();
     echo $random_numbers1;
-    echo "<br>";
+    echo "<br>0";
     echo $random_numbers1;
     echo "<br>";
     echo "<br>";
-    echo "<br>";
+    echo "<br>1";
     echo rand((int)10000000,(int)99999999);
     echo "<br>";
     echo "<br>";
-    echo "<br>";
+    echo "<br>2";
     echo rand(10000000,99999999);
