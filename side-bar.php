f1a627ce9c876fc9fe5e015c6ba85768ac3512b6
A Star Vlog <admin@astarvlog.com>
2021-01-15 19:25:13 +0800

    udpates

--- side-bar.php
+++ side-bar.php
@@ -79,23 +79,28 @@
 </li> -->
 
 <!-- Nav Item - Utilities Collapse Menu -->
-<!-- <li class="nav-item">
+<li class="nav-item">
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
         aria-expanded="true" aria-controls="collapseUtilities">
-        <i class="fas fa-fw fa-wrench"></i>
-        <span>Utilities</span>
+        <i class="fas fa-fw fa-list-alt"></i>
+        <span>Categories</span>
     </a>
     <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
         data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
+            <!-- <h6 class="collapse-header">Custom Utilities:</h6> -->
+            <?php
+                $result = mysqli_query($con,"SELECT * FROM categories");
+                $categories = mysqli_fetch_all($result,MYSQLI_ASSOC);
+                foreach($categories as $each){
+            ?>
+
+                <a class="collapse-item" href="utilities-color.html"><?php echo ucfirst($each['category']); ?></a>
+
+            <?php } ?>
         </div>
     </div>
-</li> -->
+</li>
 
     <!-- <hr class="sidebar-divider">
 
